6f0824ec1d369a8a7d4d2eb5a8930d9a4d6f61df
Federico Candiolo <candiolof@gmail.com>
2023-09-01 19:42:58 -0300

    Limpieza

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,37 +3,44 @@
 import { useState,  React} from 'react'
 import SingleForm from '../components/SingleForm'
 
-// const [pagepart, setPagepart] = useState('login')
-// console.log(pagepart)
-const pagepart = 'login';
-const pages_structure = {
-  login: {
-    title: "Iniciar Sesión",
-    submitph: "Iniciar Sesión",
-    fields: [
-      {fieldtype: "email", field: "Usuario", ph: "user@mail.com"},
-      {fieldtype: "password", field: "Contraseña"}
-    ]
-  },
-  register: {
-    title: "Registrarse",
-    submitph: "Registrarse",
-    fields: [
-      {fieldtype: "email", field: "Usuario", ph: "user@mail.com"},
-      {fieldtype: "password", field: "Contraseña"},
-      {fieldtype: "password", field: "Confirmar Contraseña"}
-    ]
-  },
-}
+//const pagepart = 'login';
+
 
 function Main() {
+    const [pagepart, setPagepart] = useState('login');
+
+    const pages_structure = {
+        login: {
+            title: "Iniciar Sesión",
+            submitph: "Iniciar Sesión",
+            fields: [
+                {fieldtype: "email", field: "Usuario", ph: "user@mail.com"},
+                {fieldtype: "password", field: "Contraseña"}
+            ]
+        },
+        register: {
+            title: "Registrarse",
+            submitph: "Registrarse",
+            fields: [
+                {fieldtype: "email", field: "Usuario", ph: "user@mail.com"},
+                {fieldtype: "password", field: "Contraseña"},
+                {fieldtype: "password", field: "Confirmar Contraseña"}
+            ]
+        },
+    };
+
+    const handleClick = () => {
+        window.alert('doing');
+        setPagepart('register')
+    };
+
     return (
         <div>
             {
                 pagepart === 'login' ?
                 <>
                     <SingleForm {...pages_structure.login}/> 
-                    <button onClick = {()=>console.log('register')} > Registrarse </button>
+                    <button type="button" onClick = {handleClick} > Registrarse </button>
                 </>
             : 
                 (
