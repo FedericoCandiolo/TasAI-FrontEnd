e1470e1d4c4dde5e343df65283c308faa551cc51
FedeCandiolo <fedecandiolo@gmail.com>
2023-09-12 19:45:56 -0300

    Pantalla de resultados armada, queda lograr que el mapa cargue dinamicamente la ubicacion, y tomar datos desde endpoint

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,63 +1,115 @@
 "use client";
 
-import { useState,  React} from 'react';
-import SingleForm from '../components/SingleForm';
-import CargaDatos from './CargaDatos';
-import RootLayout from '@/app/layout';
+import { useState, React } from "react";
+import SingleForm from "./SingleForm";
+import RootLayout from "@/app/layout";
+import CargaDatos from "./CargaDatos";
+import Resultados from "./Resultados";
 
-//const pagepart = 'login';
+export default function TestLogin() {
+  const [pagepart, setPagepart] = useState("login");
 
+  const handleRegister = () => {
+    window.alert("doing");
+    setPagepart("register");
+  };
 
-export default function Main() {
-    const [pagepart, setPagepart] = useState('login');
+  const handleLogIn = () => {
+    window.alert("Nuevo Componente");
+    setPagepart("carga");
+  };
 
-    const pages_structure = {
-        login: {
-            title: "Iniciar Sesión",
-            submitph: "Iniciar Sesión",
-            fields: [
-                {fieldtype: "email", field: "Usuario", ph: "user@mail.com"},
-                {fieldtype: "password", field: "Contraseña"}
-            ]
-        },
-        register: {
-            title: "Registrarse",
-            submitph: "Registrarse",
-            fields: [
-                {fieldtype: "email", field: "Usuario", ph: "user@mail.com"},
-                {fieldtype: "password", field: "Contraseña"},
-                {fieldtype: "password", field: "Confirmar Contraseña"}
-            ]
-        },
-    };
-
-    const handleClick = () => {
-        window.alert('doing');
-        setPagepart('register')
-    };
-
-    const handleNavigateToNewComponent = () => {
-        window.alert('Nuevo Componente');
-        setPagepart('carga');
-    }
-
-    return (
+  return (
+    <>
+      {pagepart === "login" ? (
+        <div class="h-screen flex">
+          <div class="flex w-3/4 bg-gradient-to-b from-teal-50 to-teal-800 i justify-around items-center">
             <div>
-                {
-                    pagepart === 'login' ? (
-                    <>
-                        <SingleForm {...pages_structure.login}/> 
-                        <button type="button" onClick = {handleClick} > Registrarse </button>
-                        <button type="button" onClick={handleNavigateToNewComponent}>Ir al componente nuevo</button>
-                    </>
-                ) : pagepart === 'register' ? (
-                    <SingleForm {...pages_structure.register}/> 
-                ) : pagepart === 'carga' ? (
-                    <CargaDatos />
-                ) : (
-                    <p>Not Found</p>
-                    )
-                }
+              <h1 class="text-white font-bold text-4xl font-sans">TasAI</h1>
+              <p class="text-white mt-1"> El tasador del futuro</p>
+              <button
+                type="submit"
+                class="block w-28 bg-white text-sky-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+              >
+                Read More
+              </button>
             </div>
-    )
-};
+          </div>
+          <div class="flex w-1/4 justify-center items-center bg-gradient-to-b ">
+            <form class="bg-white align-middle">
+              <h1 class="text-gray-800 font-bold text-2xl mb-1">Hola!</h1>
+              <p class="text-sm font-normal text-gray-600 mb-7">Bienvenido/a</p>
+              <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+                <input
+                  class="pl-2 outline-none border-none"
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Dirección Email"
+                />
+              </div>
+              <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <input
+                  class="pl-2 outline-none border-none"
+                  type="password"
+                  name=""
+                  id=""
+                  placeholder="Contraseña"
+                />
+              </div>
+              <button
+                type="submit"
+                class="block w-full bg-sky-800 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+                onClick={handleLogIn}
+              >
+                Iniciar Sesión
+              </button>
+              <button
+                type="submit"
+                class="block w-full bg-transparent border border-sky-800 mt-4 py-2 rounded-2xl text-sky-800 font-semibold mb-2"
+              >
+                Registrarme
+              </button>
+              <p class="text-md text-center text-sky-500 hover:text-black-500 cursor-pointer">
+                <span>Olvidé mi contraseña</span>
+              </p>
+            </form>
+          </div>
+        </div>
+      ) : pagepart === "carga" ? (
+        <CargaDatos {...{btnsubmit: ()=>setPagepart('resultados')}}/>
+      ) : pagepart === "resultados" ? (
+        <Resultados {...{resultados:{direccion: 'Av. de Mayo 866', ambientes: 3, banios: 1, toilletes: 0, cochera: false, parrilla: true, plantas: 1, m2cubiertos: 120, m2descubiertos: 80}}}/>
+        
+      ) : (
+        <p>Not Found</p>
+      )}
+    </>
+  );
+}
---
+++ src/components/Resultados.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState, React } from "react";
+import SingleForm from "./SingleForm";
+// import RootLayout from "@/app/layout";
+import FieldTasacion from './FieldTasacion';
+import Map from './Map';
+
+function Resultados({resultados}) {
+  const [pagepart, setPagePart] = useState("nuevaTasacion");
+  const [mensaje, setMensaje] = useState("");
+
+  const handleClick = () => {
+    setMensaje("Propiedad Tasada");
+  };
+
+  return (
+    <div class="flex items-centrer justify-center h-screen bg-gradient-to-b from-teal-50 to-teal-800">
+      <div class="space-y-12 space-from-header" >
+        <div class="border-b border-gray-900/10 pb-12 ">
+          <article class="box">
+            <h1>Propiedad</h1>         
+            <section class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-1 ml-2 mr-2">
+              <h3>{resultados.direccion}</h3>
+              <p>{resultados.m2cubiertos} m<sup>2</sup> cubiertos</p>
+              <p>{resultados.m2descubiertos} m<sup>2</sup> descubiertos</p>
+              <p>{resultados.ambientes} ambientes</p>
+              <p>{resultados.plantas} plantas</p>
+              <p>{resultados.banios} baños</p>
+              { resultados.toilletes && <p>{resultados.toilletes} toilletes</p> }
+              { resultados.parrilla && <p> ✔️ Parrilla </p> }
+              { resultados.cochera && <p> Cochera </p> }
+            </section>
+          </article>
+            
+        </div>
+      </div>
+
+      
+      <div style={...{width:'1000px', height: '1000px'}}/* class="mt-6 flex items-center justify-end gap-x-6 mr-2 mb-2" */>
+        <Map {...{direccion:'Avenida de Mayo 866, Buenos Aires'}}/>
+      </div>
+    </div>
+  );
+}
+
+export default Resultados;
