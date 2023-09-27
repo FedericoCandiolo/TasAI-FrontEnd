025d090b5014ea3dacd60422392253b045dcbbbb
FedeCandiolo <fedecandiolo@gmail.com>
2023-09-27 17:15:19 -0300

    Ciudad agregada, cambios para MVP1

--- src/components/CargaDatos.jsx
+++ src/components/CargaDatos.jsx
@@ -50,6 +50,67 @@ function CargaDatos({ btnsubmit , fields, setFields}) {
               {...{valor: fields["direccion"]}}{...{actualizarDato: (v)=>handleField("direccion",v)}}
               />
 
+            <FieldTasacion
+              {...{
+                fieldtype: "options",
+                classes: "sm:col-span-4 ml-2 mr-2 ",
+                cod: "direccion",
+                labeltext: "Ciudad",
+                ph: "Calle 123, Ciudad",
+                tt: "Ingrese una dirección en Argentina",
+                options: [
+                  'Barracas',
+                  'Parque Patricios',
+                  'La Boca',
+                  'Nueva Pompeya',
+                  'Villa Lugano',
+                  'Villa Soldati',
+                  'Recoleta',
+                  'Coghlan',
+                  'Villa Urquiza',
+                  'Saavedra',
+                  'Villa Puevrredon',
+                  'Belgrano',
+                  'Nuñez',
+                  'Colegiales',
+                  'Palermo',
+                  'Almagro',
+                  'Boedo',
+                  'Caballito',
+                  'Flores',
+                  'Parque Chacabuco',
+                  'Villa Crespo',
+                  'Chacarita',
+                  'Villa Ortuzar',
+                  'Agronomía',
+                  'Parque Chas',
+                  'Paternal',
+                  'Puerto Madero',
+                  'Retiro',
+                  'San Telmo',
+                  'San Nicolas',
+                  'Monserrat',
+                  'Constitucion',
+                  'San Cristobal',
+                  'Balvanera',
+                  'Liniers',
+                  'Mataderos',
+                  'Parque Avellaneda',
+                  'Villa Luro',
+                  'Monte Castro',
+                  'Floresta',
+                  'Versalles',
+                  'Villa Real',
+                  'Villa Devoto',
+                  'Villa del Parque',
+                  'Villa General Mitre',
+                  'Villa Santa Rita',
+                  'OTRA'
+                ]
+              }}
+              {...{valor: fields["ciudad"]}}{...{actualizarDato: (v)=>handleField("ciudad",v)}}
+              />
+
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 ml-2 mr-2">
               {[
                 {
